436b565c7527a5b5b082cd270f773593a2e6df51
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2024-04-18 16:41:03 +0100

    date update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <section className="py-4 pt-10">
       <div>
         <p className="text-[0.8rem] md:text-[0.85rem] leading-8 text-center text-black dark:text-gray-400">
-          Copyright &copy; 2023 Planner. All rights reserved.
+          Copyright &copy; 2024 Planner. All rights reserved.
         </p>
         {/* <p className="text-md py-1 leading-8 text-center text-gray-600 dark:text-gray-400">
           <span className="font-bold dark:text-gray-400">
