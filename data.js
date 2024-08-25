52ae5681a49313efe7b1fcce402b0385f2fc86c8
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2024-08-25 18:23:21 +0100

    new changes 2024

--- data.js
+++ data.js
@@ -10,41 +10,40 @@ import Calculator from "./ProjectImages/Calculator.jpg";
 import Plannorium from "./ProjectImages/Plannorium.jpg";
 import Xlogo from "./ProjectImages/X-logo.jpeg";
 import Edas from "./ProjectImages/edas.png";
+import home from "./ProjectImages/home.png";
 
 export const skills = [
+  "React",
+  "Next.js",
+  "Node.js",
+  "TypeScript",
+  "Tailwind CSS",
+  "Prisma",
+  "MongoDB",
+  "API Development",
+  "Django",
+  "DevOps",
   "AWS",
-  "API",
-  "SQL",
-  "JEST",
-  "YAML",
-  "SCSS",
-  "REACT",
-  "REDUX",
-  "NODEJS",
-  "NEXTJS",
-  "PYTHON",
-  "JENKINS",
-  "TAILWIND",
-  "BOOTSRAP",
-  "TYPESCRIPT",
-  "KUBERNETICS",
-  "APP ROUTER",
-  "SEVER ACTIONS",
-  "FRAMER-MOTION",
+  "Docker",
+  "CI/CD",
+  "Kubernetes",
+  "postgresql",
+  "Server Components",
+  "App Router",
 ];
 
 export const Projects = [
+  {
+    name: "JADOREIVC",
+    image: home,
+    github: "https://github.com/Mplanner12/jadoreivc",
+    demo: "https://jadoreivc-frontend.vercel.app/",
+  },
   {
     name: "PLANNORIUM ECOMMERCE",
     image: Plannorium,
     github: "https://github.com/Mplanner12/Plannorium-ecommerce",
     demo: "https://plannorium-ecommerce.vercel.app/",
-  },
-  {
-    name: "EDAS APP",
-    image: Edas,
-    github: "https://github.com/Mplanner12/EDASApp",
-    demo: "https://edas-app.vercel.app",
   },
   {
     name: "TWITTER CLONE",
@@ -53,10 +52,10 @@ export const Projects = [
     demo: "https://twitter-t3-app-two.vercel.app/",
   },
   {
-    name: "SUBSCRIPTION PACKAGES",
-    image: SubPackage,
-    github: "https://github.com/Mplanner12/Subscripton-Packages",
-    demo: "https://subscripton-packages.vercel.app/",
+    name: "EDAS APP",
+    image: Edas,
+    github: "https://github.com/Mplanner12/EDASApp",
+    demo: "https://edas-app.vercel.app",
   },
   {
     name: "PRODUCT U",
@@ -69,6 +68,12 @@ export const Projects = [
     image: RealE,
     github: "https://github.com/Mplanner12/Real_estate_project",
     demo: "https://real-estate-project-three.vercel.app/",
+  },
+  {
+    name: "SUBSCRIPTION PACKAGES",
+    image: SubPackage,
+    github: "https://github.com/Mplanner12/Subscripton-Packages",
+    demo: "https://subscripton-packages.vercel.app/",
   },
   {
     name: "PORTFOLIO V1",
