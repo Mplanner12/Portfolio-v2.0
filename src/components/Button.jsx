2cd8bc27bcf6423d3ed16c9bcdf4709d222f6203
Mustapha Shuaibu <almussanplanner12@gmail.com>
2025-09-14 22:27:36 +0100

    added resume

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,13 +8,12 @@ const Button = ({ text, className, id }) => {
   return (
     <div
       onClick={(e) => {
-        e.preventDefault(); // Stop the link from jumping instantly
-
         const target = document.getElementById("counter"); // Find the section with ID "counter"
 
         // Only scroll if we found the section and an ID is passed in
         // taht prevents the contact button from scrolling to the top
         if (target && id) {
+          e.preventDefault(); // Stop the link from jumping instantly
           const offset = window.innerHeight * 0.15; // Leave a bit of space at the top
 
           // Calculate how far down the page we need to scroll
